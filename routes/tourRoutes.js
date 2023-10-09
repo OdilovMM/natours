ed503bcc733a4151165a54c3abd60ebc84646328
OdilovMM <dssrinman@gmail.com>
2023-10-09 19:32:18 +0900

    fix: modifying tourRoutes ADDING PARAM middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,9 +2,18 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
+// ADDING PARAMETERS to ID
+router.param('id', tourController.checkID);
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
