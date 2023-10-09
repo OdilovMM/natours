a660f2a483a5bcf04f98df511d31b99732cfd19d
OdilovMM <dssrinman@gmail.com>
2023-10-09 18:00:22 +0900

    fix: modify app.js Implementing The USERS routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const app = express();
 
 // =============================================== Middleware to handle incoming function ============================================
-app.use(morgan('dev'));
+app.use(morgan('dev')); // morgan logger
 app.use(express.json());
 
 //create our own middleware ================================================================
@@ -111,6 +111,41 @@ const deleteTour = (req, res) => {
   });
 };
 
+
+//handling users route ==============================
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+
 //tours routing
 /*
 //GET METHOD ================================================= Getting all tours ====================================================
@@ -137,6 +172,18 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+//implementing Users Routes ======================================================================================================================================
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+  
+app
+  .route('api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);  
 
 // starting a server on port 3000 // ==============================================================================================
 const port = 3000;
