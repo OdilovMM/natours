b7e1d9098e21f0608c5542a3c3a5fc929915e955
OdilovMM <dssrinman@gmail.com>
2023-10-10 23:02:48 +0900

    feat: adding DataBase and connecting momngoose

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// access to mongoose and connect method
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
 
 // console.log(process.env);
 
