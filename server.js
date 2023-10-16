008b6055957500d17e7fdfaafcd0d93056d09b88
OdilovMM <dssrinman@gmail.com>
2023-10-17 00:37:38 +0900

    feat: adding a schema and creating a model

--- server.js
+++ server.js
@@ -12,13 +12,32 @@ const DB = process.env.DATABASE.replace(
 // access to mongoose and connect method
 
 mongoose
-  // .connect(process.env.DATABASE_LOCAL, {
+  // .connect(process.env.DATABASE_LOCAL, { // local database connection
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
   .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// creating a model based on the schema
+const Tour = mongoose.model('Tour', tourSchema);
 
 // console.log(process.env);
 
